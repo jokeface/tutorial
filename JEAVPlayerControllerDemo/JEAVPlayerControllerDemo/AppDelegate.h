ad55592d62cf72174ff8c71f4bfd90d131f13fb3
jokeface <511129312@qq.com>
2016-06-30 15:58:12 +0800

    1.0
    Demo的创建

---
+++ JEAVPlayerControllerDemo/JEAVPlayerControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JEAVPlayerControllerDemo
+//
+//  Created by 李佳佳 on 16/6/30.
+//  Copyright © 2016年 李佳佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
